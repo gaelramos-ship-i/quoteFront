5ba2e1c1122bff6b658cf99be2341b2fd6f38690
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-02-26 14:18:19 +0100

    first commit

---
+++ js/register.js
@@ -0,0 +1,24 @@
+const inputBtn = document.getElementById('inputBtn')
+inputBtn.addEventListener('click', async (e) => {
+  e.preventDefault(); 
+  
+  // Récupérer le contenu du input
+  const inputMail = document.getElementById('email')
+  const inputPwd = document.getElementById('password')
+  
+  const obj = {
+    email: inputMail.value,
+    password: inputPwd.value,
+  }
+  // Envoyer le contenu de mon input sur le serveur (API) méthode post
+  const datas = await fetch('http://localhost:3000/api/auth/register', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
+
+  const user = await datas.json()
+  localStorage.setItem("Token", user.token)
+})
